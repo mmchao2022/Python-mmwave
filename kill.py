24fd4abaa0917bba7aa67e8e0c14ca622007f67a
mmchao2022 <103401094+mmchao2022@users.noreply.github.com>
2022-04-15 20:20:43 +0800

    Add files via upload

---
+++ kill.py
@@ -0,0 +1,26 @@
+import os
+
+
+def killport(port):
+    command = 'sudo lsof -i:' + str(port)
+    password = '1112'
+    M = os.popen('echo %s | sudo -S %s' % (password,command)).readlines()
+    PID = M[1].split(' ')
+    for i in PID:
+        if i == '' or i == ' ':
+            PID.remove(i)
+    kill_cmd = 'sudo kill ' + PID[1]
+    os.system(kill_cmd)
+    print('The port of %s was killed',port)
+
+def main():
+    ref_port = 5220
+    tar_port = 5222
+    killport(ref_port)
+    killport(tar_port)
+
+
+if __name__ == '__main__':
+    main()
+    
+    
